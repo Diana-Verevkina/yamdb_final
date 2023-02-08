d892e42c6d1cd23fd572125f897dc221ba02f081
Diana-Verevkina <diva2208@mail.ru>
2023-02-08 18:01:41 +0300

    flake8

--- api_yamdb/api/serializers.py
+++ api_yamdb/api/serializers.py
@@ -5,8 +5,8 @@ from rest_framework import serializers
 from rest_framework.exceptions import ValidationError
 from rest_framework.generics import get_object_or_404
 from rest_framework.validators import UniqueValidator
-
 from reviews.models import Category, Comment, Genre, Review, Title, User
+
 from .validators import MixinValidatorUsername
 
 
